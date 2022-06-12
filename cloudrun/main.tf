a3be91b2aa59d22757957b4941194487647378f2
jeff <jeff@jeffbryner.com>
2022-06-12 16:24:46 -0700

    publish container

--- cloudrun/main.tf
+++ cloudrun/main.tf
@@ -54,3 +54,43 @@ resource "null_resource" "cloudbuild_cloudrun_container" {
   EOT
   }
 }
+
+
+resource "google_cloud_run_service" "default" {
+  name     = "cloudrun-srv"
+  location = "us-central1"
+
+  template {
+    spec {
+      containers {
+        image = "us-central1-docker.pkg.dev/prj-cloudrun-sample-6271/prj-containers/cloudrun"
+        env {
+          name  = "PORT"
+          value = "8080"
+        }
+      }
+    }
+  }
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+}
+
+data "google_iam_policy" "noauth" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
+  }
+}
+
+
+resource "google_cloud_run_service_iam_policy" "noauth" {
+  location = google_cloud_run_service.default.location
+  project  = google_cloud_run_service.default.project
+  service  = google_cloud_run_service.default.name
+
+  policy_data = data.google_iam_policy.noauth.policy_data
+}
