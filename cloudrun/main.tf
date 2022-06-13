2526ef58eeef24e94adf68ef3757d88103293028
jeff <jeff@jeffbryner.com>
2022-06-13 07:36:29 -0700

    correct projectid ref

--- cloudrun/main.tf
+++ cloudrun/main.tf
@@ -86,7 +86,7 @@ data "google_iam_policy" "noauth" {
 
 resource "google_cloud_run_service_iam_policy" "noauth" {
   location = google_cloud_run_service.default.location
-  project  = google_cloud_run_service.default.project
+  project  = local.project_id
   service  = google_cloud_run_service.default.name
 
   policy_data = data.google_iam_policy.noauth.policy_data
